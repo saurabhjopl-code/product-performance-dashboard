760db5e6eb5eaf2ffa7527c4f93ccb529aecc76b
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:10:04 +0530

    Update monthFilter.js

--- src/filters/monthFilter.js
+++ src/filters/monthFilter.js
@@ -1,1 +1,36 @@
-// Month Filter
+import { stateStore } from "../core/stateStore.js";
+import { eventBus } from "../core/eventBus.js";
+
+export function initMonthFilter() {
+  const root = document.getElementById("filters-root");
+
+  root.innerHTML = `
+    <div style="padding:16px 32px;">
+      <select id="month-filter">
+        <option value="">All Months</option>
+      </select>
+    </div>
+  `;
+
+  eventBus.on("DATA_LOADING_COMPLETE", () => {
+    const data = stateStore.getState().rawData.gmv.MOM;
+
+    const months = [...new Set(data.map(d => d.Month))];
+
+    const select = document.getElementById("month-filter");
+
+    months.forEach(m => {
+      const option = document.createElement("option");
+      option.value = m;
+      option.textContent = m;
+      select.appendChild(option);
+    });
+  });
+
+  root.addEventListener("change", e => {
+    if (e.target.id === "month-filter") {
+      stateStore.setFilter("month", e.target.value);
+      eventBus.emit("FILTER_CHANGED");
+    }
+  });
+}
