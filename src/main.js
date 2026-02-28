ee1a3edcf6866ab94bed51203ef2e9689fc0f301
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:57:26 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,25 +3,39 @@ import { initNavbar } from "./ui/navbar.js";
 import { initProgressBar, startProgress, finishProgress } from "./ui/progressBar.js";
 import { loadAllData } from "./core/dataLoader.js";
 import { eventBus } from "./core/eventBus.js";
+import { stateStore } from "./core/stateStore.js";
 
 import { initGMVSummary } from "./domains/gmv/summary/index.js";
 import { initMonthFilter } from "./filters/monthFilter.js";
 import { initDateFilter } from "./filters/dateFilter.js";
 import { initFilterController } from "./filters/filterController.js";
 
-
-
 document.addEventListener("DOMContentLoaded", async () => {
+
+  // UI Init
   initProgressBar();
   initNavbar();
   initLayout();
+
+  // Filters
   initMonthFilter();
   initDateFilter();
   initFilterController();
+
+  // Summary
   initGMVSummary();
-  
+
+  // Progress Listeners
   eventBus.on("DATA_LOADING_START", startProgress);
   eventBus.on("DATA_LOADING_COMPLETE", finishProgress);
 
-  await loadAllData();
+  // Start Loading
+  eventBus.emit("DATA_LOADING_START");
+
+  const data = await loadAllData();
+
+  stateStore.setRawData(data);
+  stateStore.setFilteredData(data);
+
+  eventBus.emit("DATA_LOADING_COMPLETE");
 });
