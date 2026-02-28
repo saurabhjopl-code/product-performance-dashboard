90da158f37451d22521c3951a2643c08b4e36541
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:09:16 +0530

    Update sheetConfig.js

--- src/config/sheetConfig.js
+++ src/config/sheetConfig.js
@@ -1,1 +1,6 @@
-// Central sheet configuration
+export const SHEETS = {
+  gmv: {
+    GMV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5f6lc5gca6OccttwEikVrrnarFg1nYXdOUhtE4P04kZxIlrALGewx53wfzEqahNgXS6muKbtxQt8b/pub?gid=0&single=true&output=csv",
+    MOM: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5f6lc5gca6OccttwEikVrrnarFg1nYXdOUhtE4P04kZxIlrALGewx53wfzEqahNgXS6muKbtxQt8b/pub?gid=1120345334&single=true&output=csv"
+  }
+};
