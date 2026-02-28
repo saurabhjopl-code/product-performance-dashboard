039e61e79ed898361cbf02cffb4f055e2ca4fc25
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:48:04 +0530

    Update monthFilter.js

--- src/filters/monthFilter.js
+++ src/filters/monthFilter.js
@@ -1,21 +1,23 @@
-import { stateStore } from "../core/stateStore.js";
 import { eventBus } from "../core/eventBus.js";
+import { stateStore } from "../core/stateStore.js";
 
 export function initMonthFilter() {
   const root = document.getElementById("filters-root");
 
-  root.innerHTML = `
-    <div class="filter-group">
-      <div class="filter-label">Month</div>
-      <select id="month-filter" class="filter-select">
-        <option value="">All Months</option>
-      </select>
-    </div>
+  const container = document.createElement("div");
+  container.className = "filter-group";
+
+  container.innerHTML = `
+    <div class="filter-label">Month</div>
+    <select id="month-filter" class="filter-select">
+      <option value="">All Months</option>
+    </select>
   `;
+
+  root.appendChild(container);
 
   eventBus.on("DATA_LOADING_COMPLETE", () => {
     const momData = stateStore.getState().rawData.gmv.MOM || [];
-
     const select = document.getElementById("month-filter");
 
     momData.forEach(row => {
@@ -26,7 +28,7 @@ export function initMonthFilter() {
     });
   });
 
-  root.addEventListener("change", e => {
+  container.addEventListener("change", e => {
     if (e.target.id === "month-filter") {
       stateStore.setFilter("month", e.target.value);
       eventBus.emit("FILTER_CHANGED");
