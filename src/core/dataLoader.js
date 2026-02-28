3a020e4222a13b002a77a9502a32e26d5cbc37a6
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:09:31 +0530

    Update dataLoader.js

--- src/core/dataLoader.js
+++ src/core/dataLoader.js
@@ -1,1 +1,32 @@
-// Data Loader
+import { SHEETS } from "../config/sheetConfig.js";
+import { stateStore } from "./stateStore.js";
+import { eventBus } from "./eventBus.js";
+
+export async function loadAllData() {
+  eventBus.emit("DATA_LOADING_START");
+
+  const result = {};
+
+  for (const domain in SHEETS) {
+    result[domain] = {};
+
+    for (const sheet in SHEETS[domain]) {
+      const url = SHEETS[domain][sheet];
+
+      const response = await fetch(url);
+      const text = await response.text();
+
+      const parsed = Papa.parse(text, {
+        header: true,
+        skipEmptyLines: true
+      });
+
+      result[domain][sheet] = parsed.data;
+    }
+  }
+
+  stateStore.setRawData(result);
+  stateStore.setFilteredData(result);
+
+  eventBus.emit("DATA_LOADING_COMPLETE");
+}
