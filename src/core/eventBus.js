c981f09cc7c49df2ba52787993f1e6bde86392d1
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:08:12 +0530

    Update eventBus.js

--- src/core/eventBus.js
+++ src/core/eventBus.js
@@ -1,1 +1,13 @@
-// Event Bus
+const events = {};
+
+export const eventBus = {
+  on(event, listener) {
+    if (!events[event]) events[event] = [];
+    events[event].push(listener);
+  },
+
+  emit(event, payload) {
+    if (!events[event]) return;
+    events[event].forEach(listener => listener(payload));
+  }
+};
