2176e225b4a98e7ba6bab877912f97b1b6b691f9
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 15:03:08 +0530

    Update monthFilter.js

--- src/filters/monthFilter.js
+++ src/filters/monthFilter.js
@@ -4,20 +4,19 @@ import { stateStore } from "../core/stateStore.js";
 export function initMonthFilter() {
   const root = document.getElementById("filters-root");
 
-  const container = document.createElement("div");
-  container.className = "filter-group";
+  const wrapper = document.createElement("div");
+  wrapper.className = "filter-item";
 
-  container.innerHTML = `
-    <div class="filter-label">Month</div>
-    <select id="month-filter" class="filter-select">
+  wrapper.innerHTML = `
+    <select id="month-filter" class="filter-input">
       <option value="">All Months</option>
     </select>
   `;
 
-  root.appendChild(container);
+  root.appendChild(wrapper);
 
   eventBus.on("DATA_LOADING_COMPLETE", () => {
-    const momData = stateStore.getState().rawData.gmv.MOM || [];
+    const momData = stateStore.getState().rawData.MOM || [];
     const select = document.getElementById("month-filter");
 
     momData.forEach(row => {
@@ -28,10 +27,8 @@ export function initMonthFilter() {
     });
   });
 
-  container.addEventListener("change", e => {
-    if (e.target.id === "month-filter") {
-      stateStore.setFilter("month", e.target.value);
-      eventBus.emit("FILTER_CHANGED");
-    }
+  wrapper.addEventListener("change", (e) => {
+    stateStore.setFilter("month", e.target.value);
+    eventBus.emit("FILTER_CHANGED");
   });
 }
