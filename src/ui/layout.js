874736fd2dc2f8e33bf567044e8e22473cccb1a8
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:02:58 +0530

    Update layout.js

--- src/ui/layout.js
+++ src/ui/layout.js
@@ -1,1 +1,13 @@
-// Layout
+export function initLayout() {
+  const executive = document.getElementById("executive-root");
+  const reports = document.getElementById("reports-root");
+
+  executive.innerHTML = `
+    <div class="grid grid-4" id="executive-cards"></div>
+    <div id="executive-charts" style="margin-top:32px;"></div>
+  `;
+
+  reports.innerHTML = `
+    <div id="reports-content"></div>
+  `;
+}
