67ac05bffe5c823d630318d7d9f87810f750e572
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-28 14:48:16 +0530

    Update dateFilter.js

--- src/filters/dateFilter.js
+++ src/filters/dateFilter.js
@@ -1,1 +1,31 @@
-// Date Filter
+import { eventBus } from "../core/eventBus.js";
+import { stateStore } from "../core/stateStore.js";
+
+export function initDateFilter() {
+  const root = document.getElementById("filters-root");
+
+  const container = document.createElement("div");
+  container.className = "filter-group";
+
+  container.innerHTML = `
+    <div class="filter-label">From</div>
+    <input type="date" id="start-date" class="filter-select"/>
+
+    <div class="filter-label">To</div>
+    <input type="date" id="end-date" class="filter-select"/>
+  `;
+
+  root.appendChild(container);
+
+  container.addEventListener("change", e => {
+    if (e.target.id === "start-date") {
+      stateStore.setFilter("startDate", e.target.value);
+      eventBus.emit("FILTER_CHANGED");
+    }
+
+    if (e.target.id === "end-date") {
+      stateStore.setFilter("endDate", e.target.value);
+      eventBus.emit("FILTER_CHANGED");
+    }
+  });
+}
